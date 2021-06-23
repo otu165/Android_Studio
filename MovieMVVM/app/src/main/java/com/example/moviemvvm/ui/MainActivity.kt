2ba347fc3be9ea170275d2613ba6bf196eff7736
Juyeon <otu165@naver.com>
2021-06-23 23:20:28 +0900

    fix: MultiDexError

---
+++ MovieMVVM/app/src/main/java/com/example/moviemvvm/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.moviemvvm.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.moviemvvm.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
--- MovieMVVM/app/src/main/java/com/example/moviemvvm/ui/SingleMovie.kt
+++ MovieMVVM/app/src/main/java/com/example/moviemvvm/ui/SingleMovie.kt
@@ -1,13 +1,12 @@
-package com.example.moviemvvm
+package com.example.moviemvvm.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import com.example.moviemvvm.R
 
-class MainActivity : AppCompatActivity() {
+class SingleMovie : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_single_movie)
     }
 }
